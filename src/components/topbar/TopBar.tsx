96a200a42cd41f45b44150635663d14d5dc7cf53
Paul Pan <76851428+packetsss@users.noreply.github.com>
2021-09-20 21:30:11 -0700

    improved positions of topbar

--- src/components/topbar/TopBar.tsx
+++ src/components/topbar/TopBar.tsx
@@ -30,9 +30,103 @@ export default function TopBar() {
     }
 
     window.addEventListener("scroll", scrollHandler);
-
     return (
+
         <div className="top">
+            <Navbar
+                expanded={expand}
+                fixed="top"
+                expand="md"
+                className={navColour ? "sticky" : "navbar"}
+            >
+                <Container>
+                    <Navbar.Toggle
+                        aria-controls="responsive-navbar-nav"
+                        onClick={() => {
+                            updateExpanded(expand ? false : true);
+                        }}
+                    >
+                    </Navbar.Toggle>
+                    <a className="topClickable" href="https://github.com/packetsss">
+                        <i className="topIcon fab fa-github-square"></i>
+                    </a>
+                    <a className="topClickable" href="https://www.linkedin.com/in/paul-pan001/">
+                        <i className="topIcon fab fa-linkedin"></i>
+                    </a>
+                    <a className="topClickable" href="https://www.facebook.com/paul.pan.94849/">
+                        <i className="topIcon fab fa-facebook-square"></i>
+                    </a>
+                    <a className="topClickable" href="https://twitter.com/pyj2001">
+                        <i className="topIcon fab fa-twitter-square"></i>
+                    </a>
+                    <a className="topClickable" href="https://www.instagram.com/_popaz/">
+                        <i className="topIcon fab fa-instagram-square"></i>
+                    </a>
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ml-auto" defaultActiveKey="#home">
+
+
+                            <Nav.Item>
+                                <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
+                                    <AiOutlineHome style={{ marginBottom: "2px"}} /> HOME
+                                </Nav.Link>
+                            </Nav.Item>
+
+                            <Nav.Item>
+                                <Nav.Link
+                                    as={Link}
+                                    to="/about"
+                                    onClick={() => updateExpanded(false)}
+                                >
+                                    <AiOutlineUser style={{ marginBottom: "2px" }} /> ABOUT
+                                </Nav.Link>
+                            </Nav.Item>
+
+                            <Nav.Item>
+                                <Nav.Link
+                                    as={Link}
+                                    to="/project"
+                                    onClick={() => updateExpanded(false)}
+                                >
+                                    <AiOutlineFundProjectionScreen
+                                        style={{ marginBottom: "2px" }}
+                                    /> PROJECTS
+                                </Nav.Link>
+                            </Nav.Item>
+
+                            <Nav.Item>
+                                <Nav.Link
+                                    as={Link}
+                                    to="/resume"
+                                    onClick={() => updateExpanded(false)}
+                                >
+                                    <CgFileDocument style={{ marginBottom: "2px" }} /> RESUME
+                                </Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link
+                                    as={Link}
+                                    to="/write"
+                                    onClick={() => updateExpanded(false)}
+                                >
+                                    <ImBlog style={{ marginBottom: "2px" }} /> WRITE
+                                </Nav.Link>
+                            </Nav.Item>
+
+                            <Nav.Item className="fork-btn">
+                                <Button
+                                    href="https://github.com/packetsss"
+                                    target="_blank"
+                                    className="fork-btn-inner"
+                                >
+                                    <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
+                                    <AiFillStar style={{ fontSize: "1.1em" }} />
+                                </Button>
+                            </Nav.Item>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
             {/* <div className="topLeft">
                 {<a className="topClickable" href="https://github.com/packetsss">
                     <i className="topIcon fab fa-github-square"></i>
@@ -52,103 +146,7 @@ export default function TopBar() {
             </div> */}
             <div className="topCenter">
                 <ul className="topList">
-                    <Navbar
-                        expanded={expand}
-                        fixed="top"
-                        expand="md"
-                        className={navColour ? "sticky" : "navbar"}
-                    >
-                        <Container>
-                            <Navbar.Toggle
-                                aria-controls="responsive-navbar-nav"
-                                onClick={() => {
-                                    updateExpanded(expand ? false : true);
-                                }}
-                            >
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                            </Navbar.Toggle>
-                            <Navbar.Collapse id="responsive-navbar-nav">
-                                <Nav className="ml-auto" defaultActiveKey="#home">
-                                    <a style={{ marginTop: "15px" }} className="topClickable" href="https://github.com/packetsss">
-                                        <i className="topIcon fab fa-github-square"></i>
-                                    </a>
-                                    <a style={{ marginTop: "15px" }} className="topClickable" href="https://www.linkedin.com/in/paul-pan001/">
-                                        <i className="topIcon fab fa-linkedin"></i>
-                                    </a>
-                                    <a style={{ marginTop: "15px" }} className="topClickable" href="https://www.facebook.com/paul.pan.94849/">
-                                        <i className="topIcon fab fa-facebook-square"></i>
-                                    </a>
-                                    <a style={{ marginTop: "15px" }} className="topClickable" href="https://twitter.com/pyj2001">
-                                        <i className="topIcon fab fa-twitter-square"></i>
-                                    </a>
-                                    <a style={{ marginTop: "15px" }} className="topClickable" href="https://www.instagram.com/_popaz/">
-                                        <i className="topIcon fab fa-instagram-square"></i>
-                                    </a>
 
-                                    <Nav.Item>
-                                        <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
-                                            <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
-                                        </Nav.Link>
-                                    </Nav.Item>
-
-                                    <Nav.Item>
-                                        <Nav.Link
-                                            as={Link}
-                                            to="/about"
-                                            onClick={() => updateExpanded(false)}
-                                        >
-                                            <AiOutlineUser style={{ marginBottom: "2px" }} /> About
-                                        </Nav.Link>
-                                    </Nav.Item>
-
-                                    <Nav.Item>
-                                        <Nav.Link
-                                            as={Link}
-                                            to="/project"
-                                            onClick={() => updateExpanded(false)}
-                                        >
-                                            <AiOutlineFundProjectionScreen
-                                                style={{ marginBottom: "2px" }}
-                                            />{" "}
-                                            Projects
-                                        </Nav.Link>
-                                    </Nav.Item>
-
-                                    <Nav.Item>
-                                        <Nav.Link
-                                            as={Link}
-                                            to="/resume"
-                                            onClick={() => updateExpanded(false)}
-                                        >
-                                            <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-                                        </Nav.Link>
-                                    </Nav.Item>
-                                    <Nav.Item>
-                                        <Nav.Link
-                                            as={Link}
-                                            to="/write"
-                                            onClick={() => updateExpanded(false)}
-                                        >
-                                            <ImBlog style={{ marginBottom: "2px" }} /> Write
-                                        </Nav.Link>
-                                    </Nav.Item>
-
-                                    <Nav.Item className="fork-btn">
-                                        <Button
-                                            href="https://github.com/packetsss"
-                                            target="_blank"
-                                            className="fork-btn-inner"
-                                        >
-                                            <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                                            <AiFillStar style={{ fontSize: "1.1em" }} />
-                                        </Button>
-                                    </Nav.Item>
-                                </Nav>
-                            </Navbar.Collapse>
-                        </Container>
-                    </Navbar>
                     {/* <li className="topListItem">
                         <Link className="link" to="/">HOME</Link>
                     </li>
