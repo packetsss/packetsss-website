bbcc69793b3d2ed7799b761525938a8c022180c5
Paul Pan <76851428+packetsss@users.noreply.github.com>
2022-02-22 23:05:11 -0800

    removed .vscode

--- backend/chat/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-
-from .consumers import ChatConsumer
-
-websocket_urlpatterns = [
-    # <str:room_name>
-    re_path(r"ws/chat/(?P<room_name>\w+)/$", ChatConsumer.as_asgi()),
-]
--- backend/rest_friendship/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class RestFriendshipConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "rest_friendship"
-    verbose_name = "Rest Friendship"
